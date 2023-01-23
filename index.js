a93712d64e95e78911be6d8a96d6bb522654167f
Matias Benites <matiasbenites665@gmail.com>
2023-01-23 17:38:08 -0300

    feat:product's reviews/details endpoints added

--- index.js
+++ index.js
@@ -4,10 +4,43 @@ const request = require("request-promise");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const apiKey = "7845bf7ad8dc26c62369cab28233d433";
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Amazon Scraper API!");
+});
+
+// GET Product details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET Product reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
